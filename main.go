4e39aa4ae5a5606a19a59c13b8365e6fe4ddd0a2
Ossama9 <oussama.dahbi98@gmail.com>
2024-06-21 15:14:23 +0200

    test

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "kanbghik a hbiba",
+			"messageaeaz": "test",
 		})
 	})
 	r.GET("/hello", func(c *gin.Context) {
